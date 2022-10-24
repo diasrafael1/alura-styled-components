c7504098486c8db52cccc14fbabca46b1683efa5
Rafael Dias <rafaelldiias100@gmail.com>
2022-10-24 11:53:17 -0300

    configuração dos temas.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
+import { temaClaro, temaEscuro } from "./styles/temas";
 
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
@@ -6,11 +8,11 @@ import { GlobalStyle } from "./styles/GlobalStyle";
 
 function App() {
   return (
-    <>
+    <ThemeProvider>
       <GlobalStyle />
       <Cabecalho />
       <Container />
-    </>
+    </ThemeProvider>
   );
 }
 
